3285a3bedd55e325a78598fd0ca7e8d6de3fbed5
Chengcheng Xiao <iconxicon@me.com>
2017-11-21 17:26:13 +0800

    add fit_test.py in test folder, fitting procedure successful.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # OF-4-vasp
-Geometry Optimisation and Quadratic fit program for VASP
+Geometry Optimisation and Quadratic fitting program for VASP
 
 This program was written with Python
 
@@ -10,5 +10,3 @@ This program can do:
     3.Submit Geometry optmization job.
     4.After calculation complete, check for convergence and out put "data.dat" contianing energy data and lattice parameters.
     5.Fit lattice parameters and print the optmized one (not done)
-  
-
--- test/fit_test.py
+++ test/fit_test.py
@@ -4,24 +4,41 @@ import matplotlib.pyplot as plt
 
 def main():
     # Generate Data...
-    numdata = 100
-    x = np.random.random(numdata)
+#    numdata = 100
+#    x = np.random.random(numdata)
     # X array
-    y = np.random.random(numdata)
-    z = x**2 + y**2
+#    y = np.random.random(numdata)
+#    z = x**2 + y**2
+    data_raw = np.zeros((100,100))
+    file = open("data.dat", "r")
+    content = [x.rstrip("\n") for x in file]
+    data_raw = np.array([x.split()[:5] for x in content[:]])
+    data = np.zeros(data_raw.shape)
+    #print data
+    for i in range(data_raw.shape[0]):
+        for j in range(data_raw.shape[1]):
+            data[i,j] = float(data_raw[i,j])
+    #print float(data[0,0])+float(data[0,1])
+    x = data[:,0]
+    y = data[:,1]
+    z = data[:,3]
+    z_axis = data[:,2]
+
 
     # Fit a 3rd order, 2d polynomial
     m = polyfit2d(x,y,z)
 
 
     # Evaluate it on a grid...
-    nx, ny = 40, 40
+    nx, ny = 100, 100
+    #print x.min(), x.max(),  y.min(), y.max()
     xx, yy = np.meshgrid(np.linspace(x.min(), x.max(), nx),
                          np.linspace(y.min(), y.max(), ny))
     zz = polyval2d(xx, yy, m)
 
     # Plot
-    plt.imshow(zz, extent=(x.min(), y.max(), x.max(), y.min()))
+    plt.imshow(zz, extent=(x.min()-0.2, y.max()+0.2, x.max()+0.2, y.min()-0.2))
+    # Some times withou 0.5 would fail
     plt.scatter(x, y, c=z)
     plt.show()
 
@@ -37,7 +54,9 @@ def polyfit2d(x, y, z, order=2):
             G[:,k] = x**i * y**j
     m, _, _, _ = np.linalg.lstsq(G, z)
     # I don't fully understand this, supposely a matirx trick.
-    print "Fitted result:", m[6],"*x^2+",m[2],"*y^2+",m[4],"*x*y+",m[3],"*x+",m[1],"*y+",m[0]
+    print "Fitted result:", m[6],"*x^2+",m[2],"*y^2+",m[4],"*x*y+",m[3],"*x+",m[1],"*y+",m[0],"\n"
+    #a=m[6],b=m[2],c=m[4],d=m[3],e=m[1],f=m[0]
+    print "Minimum located: x=",-(2*m[2]*m[3]-m[4]*m[1])/(4*m[6]*m[2]-m[4]*m[4])," y=",-(m[4]*m[3]-2*m[6]*m[1])/(-4*m[6]*m[2]+m[4]*m[4])
     # Print the result
     return m
 
